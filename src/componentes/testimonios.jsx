873986520b34b180eb4c8aa0f233cf4dd009703a
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-12-09 00:55:40 -0500

    FIRST COMMIT FROM VSCODE TERMINAL

---
+++ src/componentes/testimonios.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Testimonio(){
+  return(
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' src={require('../imagenes/testimonio-emma.png')} alt='Testimonio Emma'/>
+    </div>
+  );
+}
