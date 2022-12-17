26199529b2adcf04dc21f57e63a72e7e88137953
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-12-17 00:24:38 -0500

    PROJECT DONE

--- src/componentes/testimonios.jsx
+++ src/componentes/testimonios.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../stylesheets/Testimonio.css'
 
-
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio' src={require('../imagenes/testimonio-emma.png')} alt='Testimonio Emma'/>
+      <img className='imagen-testimonio' src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt='Testimonio Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
     </div>
   );
 }
+
+export default Testimonio;
